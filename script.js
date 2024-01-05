ac3fea561ba5f6f0386c2de90dd75d64aeaae5b7
Noah White <nawhite1115@gmail.com>
2024-01-04 23:40:26 -0800

    Adding class data and class names

--- script.js
+++ script.js
@@ -1,47 +1,34 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const commandInput = document.getElementById("commandInput");
-
-    // Simulate typing effect
-    const welcomeText = "";
-    const screen = document.querySelector(".screen");
-    typeText(welcomeText, screen);
-
-    function typeText(text, element) {
-        presenttext = element.innerHTML
-        let index = 0;
-        const typingInterval = setInterval(function () {
-            if (index < text.length) {
-                const code = 0x0100 + Math.random() * (0x1ff - 0x0100 + 1)
-                const char = String.fromCharCode(0x0100 + Math.random() * (0x1ff - 0x0100 + 1));
-                console.log(code)
-                const coloredChar = `<span class="typed-char">${char}</span>`;
-                element.innerHTML = presenttext + text.substring(0, index) + coloredChar;
-                index++;
-            } else {
-                clearInterval(typingInterval);
-                element.innerHTML = presenttext + text;
-            }
-        }, 50); // Adjust the typing speed (milliseconds per character) as needed
-    }
-});
-
 document.getElementById("new-employee-button").addEventListener("click", function () {
-    const strength = rollStats()
-    const speed = rollStats()
-    const intelligence = rollStats()
-    const combat = rollStats()
+    const charClassNumber = rollDie(4)
+    const charClass = classData[charClassNumber - 1]
 
-    const strength_field = document.getElementById("strength")
-    const speed_field = document.getElementById("speed")
-    const int_field = document.getElementById("intelligence")
-    const com_field = document.getElementById("combat")
-    const health_field = document.getElementById("health")
+    addClassName(charClass)
 
-    typeText(strength.toString(), strength_field, 70)
-    typeText(speed.toString(), speed_field, 70)
-    typeText(intelligence.toString(), int_field, 70)
-    typeText(combat.toString(), com_field, 70)
-    typeText((2 * strength).toString(), health_field, 70)
+    addMainStat("strength", charClass)
+    addMainStat("speed", charClass)
+    addMainStat("intelligence", charClass)
+    addMainStat("combat", charClass)
+
+    addSave("sanity", charClass)
+    addSave("fear", charClass)
+    addSave("body", charClass)
+    addSave("armor", charClass)
+
+    function addClassName(charClass) {
+        field = document.getElementById("className")
+        typeText(charClass["name"], field, 50)
+    }
+
+    function addMainStat(statName, charClass) {
+        num = rollStats()
+        field = document.getElementById(statName)
+        typeText((num + charClass["modifiers"][statName]).toString(), field, 100)
+    }
+
+    function addSave(saveName, charClass) {
+        field = document.getElementById(saveName)
+        typeText((charClass["saves"][saveName]).toString(), field, 100)
+    }
 
     function rollStats() {
         const rollTimes = 6
@@ -59,7 +46,7 @@ document.getElementById("new-employee-button").addEventListener("click", functio
         let index = 0;
         const typingInterval = setInterval(function () {
             if (index < text.length) {
-                const char = String.fromCharCode(0x0100 + Math.random() * (0x1ff - 0x0100 + 1));
+                const char = String.fromCharCode(0x0021 + Math.random() * (0x007E - 0x0021 + 1));
                 const coloredChar = `<span class="typed-char">${char}</span>`;
                 element.innerHTML = text.substring(0, index) + coloredChar;
                 index++;
@@ -67,10 +54,128 @@ document.getElementById("new-employee-button").addEventListener("click", functio
                 clearInterval(typingInterval);
                 element.innerHTML = text;
             }
-        }, speed); // Adjust the typing speed (milliseconds per character) as needed
+        }, speed);
     }
 });
 
 function rollDie(die) {
     return Math.floor(Math.random() * (die) + 1)
 }
+
+var classData = [
+    {
+        "name": "TEAMSTER",
+        "saves": {
+            "sanity": 30,
+            "fear": 35,
+            "body": 30,
+            "armor": 35
+        },
+        "code": "SC",
+        "points": 3,
+        "modifiers": {
+            "strength": 5,
+            "speed": 5,
+            "intelligence": 0,
+            "combat": 0
+        },
+        "core": {
+            "count": 2,
+            "list": [
+                "GEOLOGY",
+                "COMPUTERS",
+                "MATHEMATICS",
+                "CHEMISTRY",
+                "BIOLOGY",
+                "HYDROPONICS"
+            ]
+        }
+
+    },
+    {
+        "name": "ANDROID",
+        "saves": {
+            "sanity": 20,
+            "fear": 85,
+            "body": 40,
+            "armor": 25
+        },
+        "code": "SC",
+        "points": 3,
+        "modifiers": {
+            "strength": 0,
+            "speed": 5,
+            "intelligence": 5,
+            "combat": 0
+        },
+        "core": {
+            "count": 2,
+            "list": [
+                "GEOLOGY",
+                "COMPUTERS",
+                "MATHEMATICS",
+                "CHEMISTRY",
+                "BIOLOGY",
+                "HYDROPONICS"
+            ]
+        }
+
+    },
+    {
+        "name": "SCIENTIST",
+        "saves": {
+            "sanity": 40,
+            "fear": 25,
+            "body": 25,
+            "armor": 30
+        },
+        "code": "SC",
+        "points": 3,
+        "modifiers": {
+            "strength": 0,
+            "speed": 0,
+            "intelligence": 10,
+            "combat": 0
+        },
+        "core": {
+            "count": 2,
+            "list": [
+                "GEOLOGY",
+                "COMPUTERS",
+                "MATHEMATICS",
+                "CHEMISTRY",
+                "BIOLOGY",
+                "HYDROPONICS"
+            ]
+        }
+
+    },
+    {
+        "name": "MARINE",
+        "saves": {
+            "sanity": 25,
+            "fear": 30,
+            "body": 35,
+            "armor": 40
+        },
+        "code": "SC",
+        "points": 3,
+        "modifiers": {
+            "strength": 0,
+            "speed": 0,
+            "intelligence": 0,
+            "combat": 5
+        },
+        "core": {
+            "count": 2,
+            "list": [
+                "GEOLOGY",
+                "COMPUTERS",
+                "MATHEMATICS",
+                "CHEMISTRY",
+                "BIOLOGY",
+                "HYDROPONICS"
+            ]
+        }
+    }
+]
