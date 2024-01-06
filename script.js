d30c9f2a58cd2498b5db5cc555d834db3a173e3c
Noah White <nawhite1115@gmail.com>
2024-01-05 23:12:31 -0800

    Adding fear response + serial numbers

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+const FAST = 20;
+const MEDIUM = 50;
+const SLOW = 100;
+
 document.getElementById("new-employee-button").addEventListener("click", function () {
     const charClassNumber = rollDie(4)
     const charClass = classData[charClassNumber - 1]
 
     addClassName(charClass)
 
-    addMainStat("strength", charClass)
+    const strength = addMainStat("strength", charClass)
     addMainStat("speed", charClass)
     addMainStat("intelligence", charClass)
     addMainStat("combat", charClass)
@@ -14,20 +18,49 @@ document.getElementById("new-employee-button").addEventListener("click", functio
     addSave("body", charClass)
     addSave("armor", charClass)
 
+    addId(charClass)
+
+    addHealth(strength)
+
+    addPsychProfile(charClass)
+
     function addClassName(charClass) {
         field = document.getElementById("className")
-        typeText(charClass["name"], field, 50)
+        typeText(charClass["name"], field, MEDIUM)
     }
 
     function addMainStat(statName, charClass) {
         num = rollStats()
         field = document.getElementById(statName)
-        typeText((num + charClass["modifiers"][statName]).toString(), field, 100)
+        typeText((num + charClass["modifiers"][statName]).toString(), field, SLOW)
+
+        return (num + charClass["modifiers"][statName])
     }
 
     function addSave(saveName, charClass) {
         field = document.getElementById(saveName)
-        typeText((charClass["saves"][saveName]).toString(), field, 100)
+        typeText((charClass["saves"][saveName]).toString(), field, SLOW)
+    }
+
+    function addId(charClass) {
+        field = document.getElementById("empoyeeId")
+        typeText((charClass["code"]).toString() + Math.random().toString(36).slice(2).toUpperCase().substring(0, 7), field, MEDIUM)
+    }
+
+    function addHealth(strength) {
+        const field = document.getElementById("health")
+        typeText((strength * 2).toString(), field, SLOW)
+    }
+
+    function addPsychProfile(charClass) {
+        var psychProfile = document.getElementById("psych");
+
+        psychProfile.innerHTML = "";
+
+        var fearResponse = document.createElement("li");
+        psychProfile.appendChild(fearResponse);
+
+        typeText(charClass["fear"], fearResponse, FAST)
     }
 
     function rollStats() {
@@ -65,13 +98,14 @@ function rollDie(die) {
 var classData = [
     {
         "name": "TEAMSTER",
+        "fear": "ONCE PER SESSION, A TEAMSTER MAY RE-ROLL A ROLL ON THE PANIC EFFECT TABLE",
         "saves": {
             "sanity": 30,
             "fear": 35,
             "body": 30,
             "armor": 35
         },
-        "code": "SC",
+        "code": "TM-",
         "points": 3,
         "modifiers": {
             "strength": 5,
@@ -94,13 +128,14 @@ var classData = [
     },
     {
         "name": "ANDROID",
+        "fear": "FEAR SAVES MADE IN THE PRESENCE OF ANDROIDS HAVE DISADVANTAGE",
         "saves": {
             "sanity": 20,
             "fear": 85,
             "body": 40,
             "armor": 25
         },
-        "code": "SC",
+        "code": "AN-",
         "points": 3,
         "modifiers": {
             "strength": 0,
@@ -123,13 +158,14 @@ var classData = [
     },
     {
         "name": "SCIENTIST",
+        "fear": "WHENEVER A SCIENTIST FAILS A SANITY SAVE, EVERY FRIENDLY PLAYER NEARBY GAINS 1 STRESS",
         "saves": {
             "sanity": 40,
             "fear": 25,
             "body": 25,
             "armor": 30
         },
-        "code": "SC",
+        "code": "SC-",
         "points": 3,
         "modifiers": {
             "strength": 0,
@@ -152,13 +188,14 @@ var classData = [
     },
     {
         "name": "MARINE",
+        "fear": "WHENEVER A MARINE PANICS, EVERY FRIENDLY PLAYER NEARBY MUST MAKE A FEAR SAVE",
         "saves": {
             "sanity": 25,
             "fear": 30,
             "body": 35,
             "armor": 40
         },
-        "code": "SC",
+        "code": "MN-",
         "points": 3,
         "modifiers": {
             "strength": 0,
